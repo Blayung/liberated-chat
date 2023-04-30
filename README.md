e136ae3f24ee7f84aa80fd89dcd1b1fb233e9c64
Blayung <gpewojan1@gmail.com>
2023-04-30 22:09:58 +0200

    Move to gradle and add a warning about enforce-secure-profile on startup

--- README.md
+++ README.md
@@ -3,14 +3,12 @@
 ![**This plugin currently supports the 1.19.x version of minecraft.**](https://img.shields.io/static/v1?label=SUPPORTED%20MINECRAFT%20VERSIONS&message=1.19%20|%201.19.1%20|%201.19.2%20|%201.19.3%20|%201.19.4&color=yellowgreen&style=for-the-badge)
 # Liberated Chat Plugin
 ## What is this?
-It is a super-simple and super-lightweight minecraft plugin that disables chat reporting.
+It is probably the simplest minecraft plugin that disables chat reporting.
 
 ## Downloads
 You can download this plugin on modrinth. [https://modrinth.com/plugin/liberated-chat](https://modrinth.com/plugin/liberated-chat)
 
 ## How to compile?
 1. Clone the repo: `git clone https://github.com/Blayung/liberated-chat.git; cd liberated-chat`
-2. Install [maven](https://maven.apache.org/download.cgi)
-3. Compile the plugin's classes: `mvn compile`
-4. And assemble the jar file: `mvn assembly:single`
-5. Now the plugin jar file should be here: `./target/liberatedchat.jar`
+4. Compile the plugin: `./gradlew build`
+5. Now the plugin jar file should be here: `./build/liberated-chat-1.0.4.jar`
--- src/main/java/com/blay/liberatedchat/LiberatedChat.java
+++ src/main/java/com/blay/liberatedchat/LiberatedChat.java
@@ -1,9 +1,9 @@
 package com.blay.liberatedchat;
 
 import org.bukkit.plugin.java.JavaPlugin;
-
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.entity.Player;
 
@@ -11,13 +11,14 @@ public final class LiberatedChat extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this);
+        getLogger().warning("Make sure to disable enforce-secure-profile in server.properties!");
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.LOW)
     public void onPlayerChat(AsyncPlayerChatEvent event) {
-        String message="<"+event.getPlayer().getName()+"> "+event.getMessage();
+        String message = "<" + event.getPlayer().getName() + "> " + event.getMessage();
         getLogger().info(message);
-        for(Player player:event.getRecipients()){
+        for (Player player : event.getRecipients()) {
             player.sendMessage(message);
         }
         event.setCancelled(true);
