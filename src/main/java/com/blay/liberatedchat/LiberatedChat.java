f80ace192da24f66c1bfd33ee4a3e1264e97928e
Timon <timong.seidel@gmail.com>
2023-10-26 19:24:25 +0200

    Optimized plugin (#5)
    * Optimized message sending
    
    * Changed it so the enforce-secure-profile is automatically set to false instead of telling the user to set it
    
    * Removed teammsg command from being detected as whisper command
    teammsg as well as say and me need to be handled differently
    
    * Now uses translations instead of sending literal strings for whisper messages
    
    * little tweaks to this pr
    
    ---------
    
    Co-authored-by: Blayung <gpewojan1@gmail.com>

--- src/main/java/com/blay/liberatedchat/LiberatedChat.java
+++ src/main/java/com/blay/liberatedchat/LiberatedChat.java
@@ -1,22 +1,46 @@
 package com.blay.liberatedchat;
 
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.TranslatableComponent;
 import org.bukkit.Bukkit;
-import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.event.Listener;
-import org.bukkit.event.EventPriority;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
-import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.List;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public final class LiberatedChat extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this);
-        getLogger().warning("Make sure to disable enforce-secure-profile in server.properties, or the plugin will break!");
+
+        Path path = Paths.get(getDataFolder().getParentFile().getAbsolutePath()).getParent().resolve("server.properties");
+        try {
+            List<String> lines = Files.readAllLines(path).stream()
+                .map(line -> {
+                    if (line.startsWith("enforce-secure-profile")) {
+                        return "enforce-secure-profile=false";
+                    }
+                    return line;
+                })
+                .collect(Collectors.toList());
+
+            Files.write(path, lines, StandardOpenOption.TRUNCATE_EXISTING);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @EventHandler(priority = EventPriority.LOW)
@@ -24,16 +48,14 @@ public final class LiberatedChat extends JavaPlugin implements Listener {
         event.setCancelled(true);
         String message = "<" + event.getPlayer().getName() + "> " + event.getMessage();
         getLogger().info(message);
-        for (Player player : event.getRecipients()) {
-            player.sendMessage(message);
-        }
+        Bukkit.broadcastMessage(message);
     }
 
-    public static final List<String> whisperCommands = Arrays.asList("/w", "/minecraft:w", "/msg", "/minecraft:msg", "/tell", "/minecraft:tell", "/teammsg", "/minecraft:teammsg");
+    public static final List<String> whisperCommands = Arrays.asList("/w", "/minecraft:w", "/msg", "/minecraft:msg", "/tell", "/minecraft:tell");
 
     @EventHandler(priority = EventPriority.LOW)
     public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent event) {
-        String args[] = event.getMessage().split(" ");
+        String[] args = event.getMessage().split(" ");
         if (!whisperCommands.contains(args[0]) || args.length < 3) {
             return;
         }
@@ -53,11 +75,12 @@ public final class LiberatedChat extends JavaPlugin implements Listener {
             return;
         }
 
-        String message="";
-        for (int i=2; i<args.length; i++){
-            message += args[i];
+        StringBuilder message = new StringBuilder();
+        for (int i = 2; i < args.length; i++) {
+            message.append(args[i]);
         }
-        sender.sendMessage("§7You whisper to " + receiver.getName() + ": " + message);
-        receiver.sendMessage("§7" + sender.getName() + " whispers to you: " + message);
+
+        sender.spigot().sendMessage(new ComponentBuilder(new TranslatableComponent("commands.message.display.outgoing", receiver.getName(), message)).color(ChatColor.GRAY).create());
+        receiver.spigot().sendMessage(new ComponentBuilder(new TranslatableComponent("commands.message.display.incoming", sender.getName(), message)).color(ChatColor.GRAY).create());
     }
 }
