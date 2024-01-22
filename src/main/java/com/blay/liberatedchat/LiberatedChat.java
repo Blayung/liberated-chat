9d87f501d3c4a0f6cdad1d70f0dd28cb28913982
Blayung <gpewojan1@gmail.com>
2024-01-22 22:05:24 +0100

    Code cleanup; fixed one bug with /msg

--- src/main/java/com/blay/liberatedchat/LiberatedChat.java
+++ src/main/java/com/blay/liberatedchat/LiberatedChat.java
@@ -1,32 +1,31 @@
 package com.blay.liberatedchat;
 
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.Listener;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.entity.Player;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.chat.TranslatableComponent;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
-import org.bukkit.event.player.PlayerCommandPreprocessEvent;
-import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.util.Arrays;
 import java.util.List;
+import java.nio.file.Files;
 import java.util.stream.Collectors;
+import java.nio.file.StandardOpenOption;
+import java.io.IOException;
+import java.util.Arrays;
 
 public final class LiberatedChat extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(this, this);
-
-        Path path = Paths.get(getDataFolder().getParentFile().getAbsolutePath()).getParent().resolve("server.properties");
         try {
+            Path path = Paths.get(getDataFolder().getParentFile().getAbsolutePath()).getParent().resolve("server.properties");
+
             boolean[] replaced = {false};
 
             List<String> lines = Files.readAllLines(path).stream()
@@ -47,19 +46,22 @@ public final class LiberatedChat extends JavaPlugin implements Listener {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
     @EventHandler(priority = EventPriority.LOW)
     public void onAsyncPlayerChat(AsyncPlayerChatEvent event) {
         event.setCancelled(true);
-        getServer().broadcastMessage("<" + event.getPlayer().getName() + "> " + event.getMessage());
+        getServer().broadcastMessage('<' + event.getPlayer().getName() + "> " + event.getMessage());
     }
 
-    public static final List<String> whisperCommands = Arrays.asList("/w", "/minecraft:w", "/msg", "/minecraft:msg", "/tell", "/minecraft:tell");
+    private static final List<String> whisperCommands = Arrays.asList("/w", "/minecraft:w", "/msg", "/minecraft:msg", "/tell", "/minecraft:tell");
 
     @EventHandler(priority = EventPriority.LOW)
     public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent event) {
         String[] args = event.getMessage().split(" ");
+
         if (!whisperCommands.contains(args[0]) || args.length < 3) {
             return;
         }
@@ -67,7 +69,7 @@ public final class LiberatedChat extends JavaPlugin implements Listener {
         Player sender = event.getPlayer();
 
         if (args[1].charAt(0) == '@') {
-            sender.sendMessage("§cSelectors (@s, @a...) are not allowed in that command. It is caused by the liberated chat plugin, but we are aiming to fix it soon.");
+            sender.sendMessage("" + ChatColor.RED + "Selectors (@s, @a...) are not allowed in that command. It is caused by the liberated chat plugin, but we are aiming to fix it soon.");
             event.setCancelled(true);
             return;
         }
@@ -81,7 +83,7 @@ public final class LiberatedChat extends JavaPlugin implements Listener {
 
         StringBuilder message = new StringBuilder();
         for (int i = 2; i < args.length; i++) {
-            message.append(args[i]);
+            message.append(args[i]).append(' ');
         }
 
         sender.spigot().sendMessage(new ComponentBuilder(new TranslatableComponent("commands.message.display.outgoing", receiver.getName(), message)).color(ChatColor.GRAY).create());
